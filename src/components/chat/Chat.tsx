ee47cb8a6c8487ec8d27110a7d64424152337661
Cacao Fumiya <shikoku.fumiya@gmail.com>
2024-09-15 17:25:39 +0900

    Second

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Chat.scss";
+import ChatHeader from "./ChatHeader";
+
+const Chat = () => {
+    return <div className="chat">
+        <ChatHeader></ChatHeader>
+        <div className="chatMessage"></div>
+        <div className="chatInput"></div>
+    </div>
+}
+
+export default Chat;
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import "./Sidebar.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import SidebarChannel from "./SidebarChannel";
+import MicIcon from '@mui/icons-material/Mic';
+import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Sidebar = () => {
     return <div className="sidebar">
@@ -33,6 +36,22 @@ const Sidebar = () => {
                     <SidebarChannel></SidebarChannel>
                     <SidebarChannel></SidebarChannel>
                     <SidebarChannel></SidebarChannel>
+                </div>
+
+                <div className="sidebarFooter">
+                    <div className="sidebarAccount">
+                        <img src="./icon.png" alt="柴犬のアイコン"></img>
+                        <div className="accountName">
+                            <h4>ShinCode</h4>
+                            <span>#8162</span>
+                        </div>
+                    </div>
+
+                    <div className="sidebarVoice">
+                        <MicIcon></MicIcon>
+                        <HeadsetMicIcon></HeadsetMicIcon>
+                        <SettingsIcon></SettingsIcon>
+                    </div>
                 </div>
             </div>
         </div>
