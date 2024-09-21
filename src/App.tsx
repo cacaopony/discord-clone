847db4b4a3c1f2118c3a6ca1e8e130e44ee5df8e
cacaopony <shikoku.fumiya@gmail.com>
2024-09-21 17:04:46 +0900

    forth

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
+import { useSelector } from 'react-redux';
+import Login from './login/Login';
 
 function App() {
+
+  // const user = useSelector((state) => state.user.user);
+  const user = null;
+
   return (
     <div className="App">
-      <Sidebar></Sidebar>
-      <Chat></Chat>
+      {user ? (
+        <>
+          <Sidebar></Sidebar>
+          <Chat></Chat>
+        </>) : (
+        <>
+        <Login />
+        </>
+      )}
+
     </div>
   );
 }
